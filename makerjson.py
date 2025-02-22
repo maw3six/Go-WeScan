cb0f6861493b9ff91aa71615e16fc9f193b05b7c
Maw Maw <adikris00@protonmail.com>
2025-02-22 09:27:32 -0800

    Update makerjson.py

--- makerjson.py
+++ makerjson.py
@@ -1,11 +1,9 @@
 import json
 
-# Baca daftar path dari file
 with open("update.txt", "r", encoding="utf-8") as f:
     paths = [line.strip() for line in f if line.strip()]
 
-# Simpan dalam format JSON
-with open("paths.json", "w", encoding="utf-8") as f:
+with open("shell.json", "w", encoding="utf-8") as f:
     json.dump(paths, f, indent=2)
 
-print("Konversi selesai! File tersimpan sebagai paths.json")
+print("Convert Finish Saved At shell.json")
